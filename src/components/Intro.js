93685b68f3e76a74ec26cb030253d9f771481b80
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-06-15 09:19:56 +0530

    Added some content to the intro page

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 
 const Intro = () => {
 	return (
 		<div className="section">
-			<p>Section 1</p>
+			<Col>
+				<Row className="animate__animated animate__fadeIn">
+					<h1 className="text-center">Hey There! 👋</h1>
+				</Row>
+				<Row className="animate__animated animate__fadeIn">
+					<h1 className="text-center">I am Anu Shibin Joseph Raj 🙋‍♂️</h1>
+				</Row>
+				<Row className="animate__animated animate__fadeIn">
+					<h1 className="text-center">You can call me Shibin 😀</h1>
+				</Row>
+				<Row className="animate__animated animate__fadeIn">
+					<p className="text-center">⬇ Keep scrolling ⬇</p>
+				</Row>
+			</Col>
 		</div>
 	);
 };
