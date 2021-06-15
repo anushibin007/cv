5e018d6038eae0a0d76e6fb8ee3bb6b5902b8264
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-06-15 08:08:23 +0530

    Added FullPage framework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from "react";
 import { Container } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "animate.css/animate.min.css";
+import FullPageHolder from "./components/FullPageHolder";
 
 function App() {
-	return <Container></Container>;
+	return (
+		<Container>
+			<FullPageHolder />
+		</Container>
+	);
 }
 
 export default App;
---
+++ src/components/FullPageHolder.js
@@ -0,0 +1,23 @@
+import ReactFullpage from "@fullpage/react-fullpage";
+
+const FullPageHolder = () => {
+	return (
+		<ReactFullpage
+			scrollingSpeed={1000}
+			render={() => {
+				return (
+					<ReactFullpage.Wrapper>
+						<div className="section">
+							<p>Section 1</p>
+						</div>
+						<div className="section">
+							<p>Section 2</p>
+						</div>
+					</ReactFullpage.Wrapper>
+				);
+			}}
+		/>
+	);
+};
+
+export default FullPageHolder;
